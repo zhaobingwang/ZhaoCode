92a14b434c13dc17dbb1200137f97bae6d9f5180
No.8 <zhaobingwang@foxmail.com>
2017-08-15 22:44:51 +0800

    工作单元

---
+++ CSharp/CSharp.EntityFramework/BlogAppDAL/UOW/UnitOfWork.cs
@@ -0,0 +1,58 @@
+﻿using BlogAppDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAppDAL.UOW
+{
+    public class UnitOfWork : IDisposable
+    {
+        private readonly BlogAppContext _context = null;
+        private Repository<Blog> _blogRepository = null;
+        private Repository<Category> _categoryRepository;
+        private Repository<Comment> _commentRepository = null;
+        private Repository<Role> _roleRepository = null;
+        private Repository<User> _userRepository = null;
+
+        public UnitOfWork()
+        {
+            _context = new BlogAppContext();
+        }
+
+        public Repository<Blog> BlogRepository
+        {
+            get { return _blogRepository ?? (_blogRepository = new Repository<Blog>(_context)); }
+        }
+
+        public Repository<Category> CategoryRepository
+        {
+            get { return _categoryRepository ?? (_categoryRepository = new Repository<Category>(_context)); }
+        }
+
+        public Repository<Comment> CommentRepository
+        {
+            get { return _commentRepository ?? (_commentRepository = new Repository<Comment>(_context)); }
+        }
+        public Repository<Role> RoleRepository
+        {
+            get { return _roleRepository ?? (_roleRepository = new Repository<Role>(_context)); }
+        }
+
+        public Repository<User> UserRepository
+        {
+            get { return _userRepository ?? (_userRepository = new Repository<User>(_context)); }
+        }
+
+        public void SavaChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
