f36c4ecad64e12de42f415f0f48e9ea228703997
No.8 <zhaobingwang@foxmail.com>
2017-08-17 22:10:15 +0800

    对象和类型的一些概念

---
+++ CSharp/CSharp.Basics/ObjectsAndTypes/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectsAndTypes
+{
+    public class Car
+    {
+        private string _description;
+        private uint _nWheels;
+        public Car(string description)
+        {
+            _description = description;
+            _nWheels = 4;
+        }
+        public Car(string description, uint nWheels)
+        {
+            _description = description;
+            _nWheels = nWheels;
+        }
+    }
+
+    /// <summary>
+    /// 从构造函数中调用其他构造函数
+    /// </summary>
+    public class Car2
+    {
+        private string _description;
+        private uint _nWheels;
+
+        //以下：this关键字调用参数最匹配的那个构造函数
+        public Car2(string description):this(description,4)
+        {
+        }
+        public Car2(string description, uint nWheels)
+        {
+            _description = description;
+            _nWheels = nWheels;
+        }
+    }
+}
---
+++ CSharp/CSharp.Basics/ObjectsAndTypes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ObjectsAndTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //User user = new User();
+            //Console.WriteLine(user.Age);  //result:18
+
+            //命名的参数
+            //int z = Sum(a: 1, b: 2);
+            //Console.WriteLine(z);   //result :3
+
+            //可选参数
+            //TestMethod(10); //20
+            //TestMethod(10, 30); //40
+
+            //个数可变的参数
+            AnyNumberOfArguments(1, 2, 3, 4, 5, 6);
+        }
+
+        static void AnyNumberOfArguments(params int[] data)
+        {
+            foreach (var item in data)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void TestMethod(int a, int b = 10)
+        {
+            Console.WriteLine(a + b);
+        }
+        static int Sum(int a,int b)
+        {
+            return a + b;
+        }
+    }
+
+    class User
+    {
+        public int Age { get; set; } = 18;
+    }
+}
