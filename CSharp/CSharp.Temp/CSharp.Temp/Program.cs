b38a2f5e7a01c4ea179a776d98af5e16ae32c57c
No.8 <zhaobingwang@foxmail.com>
2017-08-25 12:36:20 +0800

    添加项目：算法

--- CSharp/CSharp.Temp/CSharp.Temp/Program.cs
+++ CSharp/CSharp.Temp/CSharp.Temp/Program.cs
@@ -10,6 +10,36 @@ namespace CSharp.Temp
     {
         static void Main(string[] args)
         {
+            //string test = "2:D121,D123;3D124;";
+            string test = "2:D121";
+            string[] arr = test.Replace('；', ';').Split(';');
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (!arr[i].Contains(":"))
+                {
+                    Console.WriteLine("异常");
+                    break;
+                }
+                string _key = arr[i].Split(':')[1];
+                string _value = arr[i].Split(':')[0];
+                string[] arrkey = _key.Replace('，', ',').Split(',');
+                for (int j = 0; j < arrkey.Length; j++)
+                {
+                    if (dict.ContainsKey(arrkey[j]))
+                    {
+                        Console.WriteLine("error");
+                        break;
+                    }
+                    dict.Add(arrkey[j], _value);
+                }
+            }
+
+
+            var testdata = "D121";
+            string result = string.Empty;
+            dict.TryGetValue(testdata, out result);
+            Console.WriteLine(result);
         }
     }
 }
