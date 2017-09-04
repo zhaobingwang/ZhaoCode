98ed317eb501c42c93a98987bba1b6e14537db8c
No.8 <zhaobingwang@foxmail.com>
2017-09-04 12:20:21 +0800

    +

---
+++ CSharp/CSharp.Temp/CSharp.Temp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Temp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "10.1235";
+            Console.WriteLine(a);
+            Console.WriteLine(decimal.Parse(a).ToString("0.000"));
+        }
+    }
+}
