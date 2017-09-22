3d0a16e07cffd461250c0744aea3271afb64df5c
No.8 <zhaobingwang@foxmail.com>
2017-09-22 16:49:00 +0800

    .

--- CSharp/CSharp.Temp/CSharp.Temp1/Program.cs
+++ CSharp/CSharp.Temp/CSharp.Temp1/Program.cs
@@ -13,6 +13,15 @@ namespace CSharp.Temp1
             string a = "10.1235";
             Console.WriteLine(a);
             Console.WriteLine(decimal.Parse(a).ToString("0.000"));
+            Console.WriteLine("********************\n");
+
+            string str = "1,2,3,";
+            string str2 = "";
+            Console.WriteLine(str.IndexOf('4'));
+
+            Console.WriteLine("a");
+            Console.WriteLine(str.Trim(','));
+            Console.WriteLine(str2.Trim(',')+"A");
         }
     }
 }
