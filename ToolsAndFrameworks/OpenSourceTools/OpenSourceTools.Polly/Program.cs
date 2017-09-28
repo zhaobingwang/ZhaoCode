aa79016496326b09b27d9d8797dbecb6e0c3d7ef
No.8 <zhaobingwang@foxmail.com>
2017-09-28 22:52:11 +0800

    弹性和瞬态故障处理库Polly

--- ToolsAndFrameworks/OpenSourceTools/OpenSourceTools.Polly/Program.cs
+++ ToolsAndFrameworks/OpenSourceTools/OpenSourceTools.Polly/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Polly;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,51 @@ namespace OpenSourceTools.Polly
     {
         static void Main(string[] args)
         {
+            try
+            {
+                //var retryTwoTimesPolicy = Policy
+                //    .Handle<DivideByZeroException>()
+                //    .Retry(3, (ex, count) =>
+                //    {
+                //        Console.WriteLine($"失败，重试次数{count}");
+                //        Console.WriteLine($"异常来自{ex.GetType().Name}");
+                //    });
+                //retryTwoTimesPolicy.Execute(() =>
+                //{
+                //    Compute();
+                //});
+
+                var politicaWaitAndRetry = Policy
+                    .Handle<DivideByZeroException>()
+                    .WaitAndRetry(new[] {
+                        TimeSpan.FromSeconds(1),
+                        TimeSpan.FromSeconds(3),
+                        TimeSpan.FromSeconds(5),
+                        TimeSpan.FromSeconds(7),
+                    }, ReportError);
+                politicaWaitAndRetry.Execute(() =>
+                {
+                    //ZeroException();
+                });
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"失败信息：{ex.Message}");
+            }
+        }
+
+        static int Compute()
+        {
+            int a = 0;
+            return 1 / a;
+        }
+        static void ZeroException()
+        {
+            throw new DivideByZeroException();
+        }
+        static void ReportError(Exception e,TimeSpan ts,int intento,Context context)
+        {
+            Console.WriteLine($"异常{intento:00}[调用耗时：{ts.Seconds}秒] [执行时间：{DateTime.Now}]");
         }
     }
 }
