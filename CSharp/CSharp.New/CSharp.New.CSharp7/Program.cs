f6f80c3fe3125833b79eb783ce75f471f212db46
No.8 <zhaobingwang@foxmail.com>
2017-10-10 17:28:49 +0800

    CSharp7-局部函数(Local functions)

--- CSharp/CSharp.New/CSharp.New.CSharp7/Program.cs
+++ CSharp/CSharp.New/CSharp.New.CSharp7/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,64 @@ namespace CSharp.New.CSharp7
     {
         static void Main(string[] args)
         {
+            Csharp7New.LocalFunctions();
+        }
+    }
+    class Csharp7New
+    {
+        /// <summary>
+        /// 局部函数(Local functions)
+        /// </summary>
+        public static void LocalFunctions()
+        {
+            Console.WriteLine("------- 局部函数 开始 -------");
+            Console.WriteLine(Add(1, 3));
+            string Add(int a, int b)
+            {
+                return $"{a}+{b}={a + b}";
+            }
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            Int64 f1 = Factorial(10000);
+            Console.WriteLine($"局部函数递归耗时:{watch.ElapsedTicks}");
+            watch.Reset();
+            watch.Start();
+            Int64 f2 = Factorial2(10000);
+            Console.WriteLine($"递归耗时:{watch.ElapsedTicks}");
+            Console.WriteLine("------- 局部函数 结束 -------");
+        }
+
+        /// <summary>
+        /// 阶乘
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static Int64 Factorial(int number)
+        {
+            if (number < 0)
+                throw new ArgumentException($"negative number:{nameof(number)}");
+            else if (number == 0)
+                return 1;
+            Int64 result = number;
+            while (number > 1)
+            {
+                Muitiply(number - 1);
+                number--;
+            }
+            void Muitiply(int x) => result *= x;
+            return result;
+        }
+        /// <summary>
+        /// 阶乘2
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        private static Int64 Factorial2(int number)
+        {
+            if (number < 0)
+                throw new ArgumentException("nagative number", nameof(number));
+            return number == 0 ? 1 : number * Factorial2(number - 1);
         }
     }
 }
