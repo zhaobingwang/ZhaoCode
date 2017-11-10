1020943426ff56d74d8b826b8985c3c6e335556b
No.8 <zhaobingwang@foxmail.com>
2017-11-10 12:34:30 +0800

    webapi返回json,不返回soap

--- Web/WebAPI/WebAPI/App_Start/WebApiConfig.cs
+++ Web/WebAPI/WebAPI/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace WebAPI
@@ -10,7 +11,7 @@ namespace WebAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
             // Web API routes
             config.MapHttpAttributeRoutes();
 
