81350ac6d7388278db495f737ad90b1ad129edf6
No.8 <zhaobingwang@foxmail.com>
2017-11-21 15:55:48 +0800

    temp-console字体颜色

--- CSharp/CSharp.Temp/CSharp.Temp1/Program.cs
+++ CSharp/CSharp.Temp/CSharp.Temp1/Program.cs
@@ -10,24 +10,23 @@ namespace CSharp.Temp1
     {
         static void Main(string[] args)
         {
-            //string a = "10.1235";
-            //Console.WriteLine(a);
-            //Console.WriteLine(decimal.Parse(a).ToString("0.000"));
-            //Console.WriteLine("********************\n");
-
-            //string str = "1,2,3,";
-            //string str2 = "";
-            //Console.WriteLine(str.IndexOf('4'));
-
-            //Console.WriteLine("a");
-            //Console.WriteLine(str.Trim(','));
-            //Console.WriteLine(str2.Trim(',')+"A");
-            var key=GetKeyFromDict("AGE");
-            if (string.IsNullOrEmpty(key))
-            {
-                Console.WriteLine("1");
-            }
-            Console.WriteLine(key);
+            //https://www.cnblogs.com/mq0036/p/3707474.html
+            //Console.BackgroundColor = ConsoleColor.Blue; //设置背景色
+            //Console.ForegroundColor = ConsoleColor.White; //设置前景色，即字体颜色
+            //Console.WriteLine("第一行白蓝.");
+            //Console.ResetColor(); //将控制台的前景色和背景色设为默认值
+            //Console.BackgroundColor = ConsoleColor.Green;
+            //Console.ForegroundColor = ConsoleColor.DarkGreen;
+            //string str = "第三行 绿暗绿";
+            //Console.WriteLine(str.PadRight(Console.BufferWidth - (str.Length % Console.BufferWidth))); //设置一整行的背景色
+            //Console.ResetColor();
+            Console.Write("这是一个");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("测试");
+            Console.ResetColor();
+            Console.Write("内容");
+            Console.ResetColor();
+            Console.Read();
         }
         public static string GetKeyFromDict(string sourcekey)
         {
