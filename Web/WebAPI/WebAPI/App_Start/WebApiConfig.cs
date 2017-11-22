2116442811ecdc0db60d98aeb3b2682023dddfbd
No.8 <zhaobingwang@foxmail.com>
2017-11-22 21:05:55 +0800

    取消访问地址/api/controller前的api

--- Web/WebAPI/WebAPI/App_Start/WebApiConfig.cs
+++ Web/WebAPI/WebAPI/App_Start/WebApiConfig.cs
@@ -17,7 +17,7 @@ namespace WebAPI
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
