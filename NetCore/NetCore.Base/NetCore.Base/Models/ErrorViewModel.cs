b844244389654a7aa7a2236b74b62602cd97f733
No.8 <zhaobingwang@foxmail.com>
2017-11-23 19:29:30 +0800

    +

---
+++ NetCore/NetCore.Base/NetCore.Base/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCore.Base.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
