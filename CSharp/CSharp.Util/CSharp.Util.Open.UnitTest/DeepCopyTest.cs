63284e630a7e3b172bb616e280e503875a88969b
No.8 <zhaobingwang@foxmail.com>
2017-12-04 19:27:56 +0800

    3种对象深拷贝及其单元测试

---
+++ CSharp/CSharp.Util/CSharp.Util.Open.UnitTest/DeepCopyTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp.Util.Open.UnitTest
+{
+    [TestClass]
+    public class DeepCopyTest
+    {
+        [TestMethod]
+        public void DeepCopyTest1()
+        {
+            User u1 = new User();
+            u1.UName = "张三";
+            User u2 = DeepCopy.DeepCopyByReflect(u1);
+            Assert.AreNotEqual(u1, u2);
+        }
+    }
+    public class User
+    {
+        public string UName { get; set; }
+    }
+}
--- CSharp/CSharp.Util/CSharp.Util.Open/DeepCopy.cs
+++ CSharp/CSharp.Util/CSharp.Util.Open/DeepCopy.cs
@@ -5,26 +5,13 @@ using System.Linq;
 using System.Reflection;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 
-namespace CSharp.Temp
+namespace CSharp.Util.Open
 {
-    class Program
+    public class DeepCopy
     {
-        static void Main(string[] args)
-        {
-            User user = new User();
-            user.Name = "张三";
-            //User user2 = user;//DeepCopyByReflect<User>(user);
-            User user2 = DeepCopyByReflect<User>(user);
-            user2.Name = "李四";
-            Console.WriteLine(user.Name);
-            Console.WriteLine(user2.Name);
-
-        }
-        //四种深拷贝方法
         public static T DeepCopyByReflect<T>(T obj)
         {
             //如果是字符串或值类型则直接返回
@@ -69,10 +56,5 @@ namespace CSharp.Temp
             }
             return (T)retval;
         }
-    }
-    public class User
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
     }
 }
