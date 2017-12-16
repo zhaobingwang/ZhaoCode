ba4483083d5f6863faed62a334c1f1a8dffdc16b
No.8 <zhaobingwang@foxmail.com>
2017-12-16 15:22:59 +0800

    单点登录(SSO)原理

--- CSharp/CSharp.Unclassified/CSharp.Unclassified.SSODemo_Client2/Controllers/HomeController.cs
+++ CSharp/CSharp.Unclassified/CSharp.Unclassified.SSODemo_Client2/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,8 +10,40 @@ namespace CSharp.Unclassified.SSODemo_Client2.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public static List<string> Tokens = new List<string>();
+        public async Task<ActionResult> Index()
         {
+            var tokenId = Request["tokenId"];
+            //如果tokenId不为空，则是由Service302过来的。
+            if (tokenId != null)
+            {
+                using (HttpClient http = new HttpClient())
+                {
+                    //验证Tokend是否有效
+                    var isValid = await http.GetStringAsync($"http://localhost:8301/Home/TokenIdIsValid?tokenId={tokenId}");
+                    if (bool.Parse(isValid.ToString()))
+                    {
+                        if (!Tokens.Contains(tokenId))
+                        {
+                            //记录登陆过的client (主要是为了可以统一登出)
+                            Tokens.Add(tokenId);
+                        }
+                        Session["token"] = tokenId;
+                    }
+                }
+            }
+            //判断是否是登录状态
+            if (Session["token"] == null || !Tokens.Contains(Session["token"].ToString()))
+            {
+                return Redirect("http://localhost:8301/Home/Verification?backUrl=http://localhost:8303/Home");
+            }
+            else
+            {
+                if (Session["token"] != null)
+                {
+                    Session["token"] = null;
+                }
+            }
             return View();
         }
 
--- CSharp/CSharp.Unclassified/CSharp.Unclassified.SSODemo_Service/Controllers/HomeController.cs
+++ CSharp/CSharp.Unclassified/CSharp.Unclassified.SSODemo_Service/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,6 +10,7 @@ namespace CSharp.Unclassified.SSODemo_Service.Controllers
 {
     public class HomeController : Controller
     {
+        public static Dictionary<string, Guid> TokenIds = new Dictionary<string, Guid>();
         public ActionResult Index()
         {
             return View();
@@ -25,6 +28,80 @@ namespace CSharp.Unclassified.SSODemo_Service.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        /// <summary>
+        /// 验证是否登录
+        /// </summary>
+        /// <param name="backUrl"></param>
+        /// <returns></returns>
+        public ActionResult Verification(string backUrl)
+        {
+            if (Session["user"]==null)
+            {
+                return Redirect($"Login?backUrl=" + backUrl);
+            }
+            Session["user"] = "已经登录";
+            return Redirect($"{backUrl}?tokenId=" + TokenIds[Session.SessionID]);
+        }
+
+        /// <summary>
+        /// 验证tokenid是否有效
+        /// </summary>
+        /// <param name="tokenId"></param>
+        /// <returns></returns>
+        public bool TokenIdIsValid(Guid tokenId)
+        {
+            return TokenIds.Any(t => t.Value == tokenId);
+        }
+
+        /// <summary>
+        /// 返回登录页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="password"></param>
+        /// <param name="backUrl"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public string Login(string name, string password, string backUrl)
+        {
+            if (true) //TODO：验证用户名密码登录
+            {
+                //用Session标识会话是登录状态
+                Session["user"] = "XXX已登录";
+                //在认证中心 保存客户端Client的登录认证码
+                TokenIds.Add(Session.SessionID, Guid.NewGuid());
+            }
+            else //验证失败重新登录
+            {
+                return "/Home/Login";
+            }
+            return backUrl + "?tokenId=" + TokenIds[Session.SessionID]; //生成一个tokenId 发放到客户端
+        }
+
+        /// <summary>
+        /// 退出
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ActionResult> Logout()
+        {
+            using (HttpClient http=new HttpClient())
+            {
+                await http.GetAsync($"http://localhost:8302/Home/ClearToken?tokenId={TokenIds[Session.SessionID]}");   //client1
+                await http.GetAsync($"http://localhost:8303/Home/ClearToken?tokenId={TokenIds[Session.SessionID]}");   //client2
+            }
+            TokenIds.Remove(Session.SessionID);
+            Session["user"] = null;
+            return Redirect("Login");
         }
     }
 }
