f5fefc72c1a8a2a50fa2ee2f7fca160426b8beba
No.8 <zhaobingwang@foxmail.com>
2017-12-26 12:00:06 +0800

    +

---
+++ ToolsAndFrameworks/QuartzNet/QuartzNetCoreConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuartzNetCoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
