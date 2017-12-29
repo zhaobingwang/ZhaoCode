e01b0ce885ebd2dc47c2ecfc71192d6550b2a22e
No.8 <zhaobingwang@foxmail.com>
2017-12-29 12:42:39 +0800

    log4net配置修改

---
+++ ToolsAndFrameworks/Log4Net/Log4Net.ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4Net.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure(); //读取log4net配置信息
+            try
+            {
+                int a = 0;
+                int b = 10 / a;
+            }
+            catch (Exception ex)
+            {
+                ILog logger = LogManager.GetLogger("errorMsg");
+                logger.Error(ex.ToString());
+            }
+        }
+    }
+}
---
+++ ToolsAndFrameworks/Log4Net/Log4Net.ConsoleApp/log4net.md
@@ -0,0 +1,4 @@
+﻿#
+
+## log4net 配置：http://logging.apache.org/log4net/release/config-examples.html
+
