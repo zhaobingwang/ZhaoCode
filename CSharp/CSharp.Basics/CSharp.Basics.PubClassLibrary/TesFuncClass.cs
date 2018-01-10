24bf268f0a2fcb2314ac386ad45f83bff85d3027
No.8 <zhaobingwang@foxmail.com>
2018-01-10 19:32:30 +0800

    +

---
+++ CSharp/CSharp.Basics/CSharp.Basics.PubClassLibrary/TesFuncClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Basics.PubClassLibrary
+{
+    public class TesFuncClass
+    {
+        public static string SayHi(string name, int age, string location)
+        {
+            return $"I'm {name}, {age} years old,from {location}";
+        }
+    }
+}
---
+++ CSharp/CSharp.Basics/UnitTestProject/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTestProject
+{
+    public class TestReflector
+    {
+
+        [Fact]
+        public void Reflector_Test()
+        {
+            
+        }
+    }
+}
