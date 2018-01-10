6ac3c520c3ebc3c54931a4ea592620d491c969c0
No.8 <zhaobingwang@foxmail.com>
2018-01-10 22:05:34 +0800

    反射性能测试

---
+++ CSharp/CSharp.Basics/UnitTestProject/ReflectorTests.cs
@@ -0,0 +1,46 @@
+﻿using CSharp.Basics.PubClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTestProject
+{
+    public class TestReflector
+    {
+
+        [Fact]
+        public void Direct_Test()
+        {
+            var success = true;
+            for (int i = 0; i < 1000000; i++)
+            {
+                var instance = new TesFuncClass();
+                if (instance == null)
+                {
+                    success = false;
+                }
+            }
+
+            Assert.True(success);
+        }
+        [Fact]
+        public void Reflector_Test()
+        {
+            var success = true;
+            for (int i = 0; i < 1000000; i++)
+            {
+                var instance = Assembly.Load("CSharp.Basics.PubClassLibrary").CreateInstance("CSharp.Basics.PubClassLibrary.TesFuncClass");
+                if (instance==null)
+                {
+                    success = false;
+                }
+            }
+
+            Assert.True(success);
+        }
+    }
+}
