56bf1ac18de67c2b7ffca9b7c50a1d45921de44f
No.8 <zhaobingwang@foxmail.com>
2018-01-19 12:32:53 +0800

    Task

---
+++ CSharp/CSharp.Task/ExampleCore/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExampleCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task taskA = Task.Run(() => Thread.Sleep(3000));
+            Console.WriteLine($"taskA Status:{taskA.Status}");
+            try
+            {
+                taskA.Wait(1000);
+                bool completed = taskA.IsCompleted;
+                Console.WriteLine($"taskA completed:{completed},Status：{taskA.Status}");
+                if (!completed)
+                    Console.WriteLine("Tiem out before taksA completed");
+            }
+            catch (AggregateException)
+            {
+                Console.WriteLine("Exception in taskA");
+            }
+        }
+    }
+}
