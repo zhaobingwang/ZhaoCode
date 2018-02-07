4d3c963abad151f602df2e39fee1cd84220a02f2
No.8 <zhaobingwang@foxmail.com>
2018-02-07 13:01:02 +0800

    Csharp7新特性-out变量

---
+++ CSharp/CSharp.New/CSharp7/Csharp7.Out/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Csharp7.Out
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========Csharp7之前out的用法========");
+            Console.WriteLine("请输入一个数字：");
+            string input = Console.ReadLine();
+            int numericResult;
+            if (int.TryParse(input, out numericResult))
+            {
+                Console.WriteLine(numericResult);
+            }
+            else
+            {
+                Console.WriteLine("Could not parse input");
+            }
+
+            Console.WriteLine("========Csharp7中out的用法========");
+            Console.WriteLine("请输入一个数字：");
+            input = Console.ReadLine();
+            if (int.TryParse(input, out int result))
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Could not parse input");
+            }
+
+            Console.WriteLine($"convert:{TypeHandler.ParseInt32(input)}");
+        }
+    }
+
+    public class TypeHandler
+    {
+        public static int? ParseInt32(string s)
+        {
+            if (!int.TryParse(s, out int result))
+            {
+                return null;
+            }
+            return result;
+        }
+    }
+    public class Extension
+    {
+
+    }
+}
