fda92c894f791bae64afd1bd9189559f1f5a373d
No.8 <zhaobingwang@foxmail.com>
2018-02-07 14:52:52 +0800

    Csharp7-元组

---
+++ CSharp/CSharp.New/CSharp7/Csharp7.Tuple/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Csharp7.Tuple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (int x,int y) point = (1, 2);
+            Console.WriteLine($"x:{point.x},y:{point.y}");
+        }
+    }
+}
