028d8b7e52196e54fcc28614935e1bd29fd43156
No.8 <zhaobingwang@foxmail.com>
2018-02-26 11:27:26 +0800

    ref和out

---
+++ Practice/RefAndOut/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RefAndOut
+{
+    class Program
+    {
+        static void Method(ref int i)
+        {
+            i = 1;
+        }
+        static void Method2(out int i)
+        {
+            i =2;
+        }
+        static void Main(string[] args)
+        {
+            int val=0;
+            Method(ref val);
+            System.Console.WriteLine(val);
+
+            int val2;
+            Method2(out val2);
+            System.Console.WriteLine(val2);
+        }
+    }
+}
