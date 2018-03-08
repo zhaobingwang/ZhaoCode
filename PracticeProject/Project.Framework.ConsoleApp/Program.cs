f66b66d4174ba2f07402b9251e2ba36e871183a6
No.8 <zhaobingwang@foxmail.com>
2018-03-08 23:43:41 +0800

    生产测试数据

--- PracticeProject/Project.Framework.ConsoleApp/Program.cs
+++ PracticeProject/Project.Framework.ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Project.Framework.Data;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,14 +12,45 @@ namespace Project.Framework.ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("开始...");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 2; i++)
+            {
+                Task t1 = ProduceData(10, 10, i.ToString() + "-A");
+                Task t2 = ProduceData(10, 10, i.ToString() + "-B");
+                Task t3 = ProduceData(10, 10, i.ToString() + "-C");
+                Task t4 = ProduceData(10, 10, i.ToString() + "-D");
+                Task t5 = ProduceData(10, 10, i.ToString() + "-E");
+            }
+            sw.Stop();
+            Console.WriteLine($"结束,耗时：{sw.Elapsed}");
+
+        }
+
+        static async Task ProduceData(int count1, int count2, string taskId)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
             using (var db = new MySqlContext())
             {
-                var users = db.Users;
-                foreach (var item in users)
+                List<User> userList = new List<User>();
+                for (int i = 0; i < count1; i++)
                 {
-                    Console.WriteLine(item.Name);
+                    for (int j = 0; j < count2; j++)
+                    {
+                        var user = new User();
+                        user.Name = ChineseName.RandomName();
+                        System.Threading.Thread.Sleep(10);
+                        userList.Add(user);
+                    }
                 }
+                db.Users.AddRange(userList);
+                await db.SaveChangesAsync();
+                Console.WriteLine($"当前已有{db.Users.Count()}条记录");
             }
+            sw.Stop();
+            Console.WriteLine($"#{taskId} 耗时：{sw.Elapsed}");
         }
     }
 }
