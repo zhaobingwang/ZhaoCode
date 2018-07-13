ae6ce2df889104184df63bf03e8b71d6e7f64d06
Route8 <zhaobingwang@foxmail.com>
2018-07-13 15:16:14 +0800

    delegate

---
+++ CSharp/CSharp.Basics/DelegateAndEvents/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace DelegateAndEvents
+{
+    class Program
+    {
+        static int Double(int x) { return x * 2; }
+        static int Square(int x) { return x * x; }
+
+        
+        static void Main(string[] args)
+        {
+            //Example example = new Example();
+            //example.Datafiltering();
+
+            int[] values = { 1, 2, 3, 4, 5 };
+            //Example.Calculate(values, Double);
+            Example.Calculate(values, Square);
+            foreach (var item in values)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    public delegate int Calculator(int x);
+    public class Example
+    {
+        public void Datafiltering()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Func<int, bool> foo = tmp => tmp > 5;
+            var list = arr.Where(foo).ToList();
+            // or
+            var list2 = arr.Where(x => x > 5);
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void Calculate(int[] values, Calculator calculator)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                values[i] = calculator(values[i]);
+            }
+        }
+    }
+}
---
+++ CSharp/CSharp.Basics/EventsDemo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace EventsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Publisher pub = new Publisher();
+            Subscriber sub1 = new Subscriber("sub1", pub);
+            Subscriber sub2 = new Subscriber("sub2", pub);
+            pub.DoSomething();
+            Console.Read();
+        }
+    }
+
+    // Define a class to hold custom event info
+    public class CustomEventArgs : EventArgs
+    {
+        public CustomEventArgs(string s)
+        {
+            Message = s;
+        }
+        public string Message { get; set; }
+    }
+
+    // Class that publishes an event
+    class Publisher
+    {
+        public event EventHandler<CustomEventArgs> RaiseCustomEvent;
+        public void DoSomething()
+        {
+            OnRaiseCustomEvent(new CustomEventArgs("Did something"));
+        }
+
+        protected virtual void OnRaiseCustomEvent(CustomEventArgs e)
+        {
+            EventHandler<CustomEventArgs> handler = RaiseCustomEvent;
+            if (handler != null)
+            {
+                e.Message += $" at {DateTime.Now.ToString()}";
+                handler(this, e);
+            }
+        }
+    }
+
+    class Subscriber
+    {
+        private string id;
+        public Subscriber(string ID, Publisher pub)
+        {
+            id = ID;
+            pub.RaiseCustomEvent += HandleCustomEvent;
+        }
+        void HandleCustomEvent(object sender, CustomEventArgs e)
+        {
+            Console.WriteLine($"{id} received this message: {e.Message}");
+        }
+    }
+}
