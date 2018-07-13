33c69bde7861075637a72ccbc743b1ae5d4f33e4
Route8 <zhaobingwang@foxmail.com>
2018-07-13 16:13:30 +0800

    观察者模式

---
+++ CSharp/CSharp.Basics/Observer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Heater heater = new Heater();
+            Alarm alarm = new Alarm();
+            heater.BoilEvent += alarm.MakeAlert;
+            //heater.BoilEvent += (new Alarm()).MakeAlert;
+            heater.BoilEvent += Display.ShowMsg;
+            heater.BoilWater();
+        }
+    }
+    public class Heater
+    {
+        private int temperature;
+        public delegate void BoilHandler(int param);
+        public event BoilHandler BoilEvent;
+
+        public void BoilWater()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                temperature = i;
+                if (temperature > 95)
+                {
+                    BoilEvent?.Invoke(temperature);
+                }
+            }
+        }
+    }
+    public class Alarm
+    {
+        public void MakeAlert(int param)
+        {
+            Console.WriteLine($"Alarm:当前水温：{param}℃");
+        }
+    }
+
+    public class Display
+    {
+        public static void ShowMsg(int param)
+        {
+            Console.WriteLine($"Display:当前水温：{param}℃");
+        }
+    }
+}
