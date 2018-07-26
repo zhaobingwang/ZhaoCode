6560c094f883a71b0f1086a27329de1c2d161291
Route8 <zhaobingwang@foxmail.com>
2018-07-26 21:42:35 +0800

    构造函数注入

---
+++ DesignConcept/Ioc/DI.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ioc.DI
+{
+    public class DI
+    {
+        static void Main(string[] args)
+        {
+            SqlServerDal dal = new SqlServerDal();  // 在外部创建依赖对象
+            Order order = new Order(dal);   // 通过构造函数注入依赖
+            order.Add();
+            Console.Read();
+        }
+    }
+
+    public interface IDataAccess
+    {
+        void Add();
+    }
+    public class SqlServerDal : IDataAccess
+    {
+        public void Add()
+        {
+            Console.WriteLine("在SqlServer数据库中添加一条订单");
+        }
+    }
+    public class MySqlDal : IDataAccess
+    {
+        public void Add()
+        {
+            Console.WriteLine("在MySql数据库中添加一条订单");
+        }
+    }
+
+    public class Order
+    {
+        private IDataAccess _ida;   // 定义一个私有变量保存抽象
+        public Order(IDataAccess ida)
+        {
+            _ida = ida; // 依赖注入
+        }
+        public void Add()
+        {
+            _ida.Add();
+        }
+    }
+}
---
+++ DesignConcept/Ioc/WithOutDI.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Ioc.WithOutDI
+{
+    class WithOutDI
+    {
+        static void Main(string[] args)
+        {
+            // without DI
+            Order order = new Order();
+            order.Add();
+            Console.ReadLine();
+
+        }
+    }
+
+
+    #region 数据库读写
+    /// <summary>
+    /// sql server 数据库读写
+    /// </summary>
+    public class SqlServerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("在SqlServer数据库中添加一条订单");
+        }
+    }
+    public class MySqlDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("在MySql数据库中添加一条订单");
+        }
+    }
+    #endregion
+
+    #region 订单逻辑
+    //public class Order
+    //{
+    //    private readonly SqlServerDal dal = new SqlServerDal();
+    //    public void Add()
+    //    {
+    //        dal.Add();
+    //    }
+    //}
+
+    // SqlServerDal换成MySqlDal
+    public class Order
+    {
+        private readonly MySqlDal dal = new MySqlDal();
+        public void Add()
+        {
+            dal.Add();
+        }
+    }
+    #endregion
+}
