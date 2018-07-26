8968c133bda8b8e50775c3594f9355aee1488ec4
Route8 <zhaobingwang@foxmail.com>
2018-07-26 15:12:05 +0800

    控制器缓存

---
+++ PracticeProject/Project.WebMVC/Controllers/CacheExampleController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.WebMVC.Controllers
+{
+    public class CacheExampleController : Controller
+    {
+        // GET: CacheExample
+        [OutputCache(Duration = 10, VaryByParam = "none")]
+        public ActionResult Index()
+        {
+            ViewBag.Test = "测试内容";
+            ViewBag.Now = DateTime.Now.ToString();
+            return View();
+        }
+
+        [OutputCache(Duration = 10, VaryByParam = "name")]
+        public ActionResult GetName(string name)
+        {
+            ViewBag.Name = name;
+            ViewBag.Now = DateTime.Now.ToString();
+            return View();
+        }
+    }
+}
