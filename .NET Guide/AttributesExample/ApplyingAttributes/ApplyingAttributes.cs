ac60670d9cdae3d6fcd27ae3f7a773237e4e4ad2
Route8 <zhaobingwang@foxmail.com>
2018-07-26 08:48:03 +0800

    使用内置特性

---
+++ .NET Guide/AttributesExample/ApplyingAttributes/ApplyingAttributes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ApplyingAttributes
+{
+    class ApplyingAttributes
+    {
+        static void Main(string[] args)
+        {
+            int i = Example.Add(1, 2);
+            Console.WriteLine(i);
+        }
+    }
+    class Example
+    {
+        [Obsolete("将在下一个版本中删除")]
+        public static int Add(int a, int b)
+        {
+            return (a + b);
+        }
+    }
+}
