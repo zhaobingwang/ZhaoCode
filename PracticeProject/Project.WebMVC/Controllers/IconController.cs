98c3be91f268bf89107213edc0652e1f490668da
Route8 <zhaobingwang@foxmail.com>
2018-07-26 13:54:21 +0800

    alibaba 图标库

---
+++ PracticeProject/Project.WebMVC/Controllers/IconController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.WebMVC.Controllers
+{
+    public class IconController : Controller
+    {
+        // GET: Icon
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
